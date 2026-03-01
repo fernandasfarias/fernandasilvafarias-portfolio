79725f68e9361b7a8880dc1f2556aa89117cfd6a
fernandasfarias <fernandafariass734@gmail.com>
2026-03-01 16:03:19 -0300

    feat(contact): create social media card

--- app/components/sections/Contact.tsx
+++ app/components/sections/Contact.tsx
@@ -2,12 +2,13 @@ import { MdOutlineEmail } from "react-icons/md"; // email icon
 import { FiGithub } from "react-icons/fi"; // github icon
 import { LuLinkedin } from "react-icons/lu"; // linkedin icon
 import { FiInstagram } from "react-icons/fi"; // instagram icon
+import Link from "next/link";
 
 export default function(){
     return(
         <div id="contact"
         className="min-h bg-[#fff5f9] dark:bg-[#120a1f]
-        flex flex-col items-center justify-center scroll-mt-20 w-full p-8 relative">
+        flex flex-col items-center justify-center scroll-mt-20 w-full p-8 px-12 md:px-8 relative">
             
             {/* title - FINISHED*/}
             <h2 className="text-4xl md:text-6xl mb-2 sticky top-0 font-bold
@@ -22,49 +23,82 @@ export default function(){
             {/* note !!!: items-start prevents the two containers from growing together */}
 
                 {/* first container -> FINISHED*/}
-                <div className="p-8 rounded-xl bg-[#ede4ff] text-black/80">
+                <div className="p-8 rounded-xl text-black/80
+                border-2 border-[#dbc1ef] dark:border-[#c77dff]
+                bg-gradient-to-r from-[#ecd9ff]/50 via-[#d8c4f5]/50 to-[#cfe9ff]/50
+                dark:text-white">
+
                     <h2 className="text-xl font-semibold mb-2">Contact me</h2>
 
                     <h3 className="text-normal mb-5">Feel free to contact me on my social media</h3>
 
-                    <div className="flex items-center gap-2 hover:underline hover:text-[#c77dff]">
+                    <div className="flex items-center gap-2 hover:underline hover:text-[#c77dff] dark:hover:text-[#cfe9ff]">
                         <MdOutlineEmail size={25}/>
                         <p className="text-normal">fernandafariass734@gmail.com</p>
                     </div>
                 </div>  
 
-                {/* second container */}
-                <div className="p-8 rounded-xl bg-[#ffffff] text-black/80
-                flex flex-col items-center justify-center gap-6">
+                {/* second container - FINISHED */}
+                <div className="p-4 pb-8 rounded-xl bg-[#ffffff] text-black/80
+                flex flex-col items-center justify-center gap-6 
+                border-2 border-[#dbc1ef] dark:border-[#c77dff]">
 
-                    <h2 className="">Social Media</h2>
+                    <h2 className="text-xl font-semibold mb-2">Social Media</h2>
 
                     {/* little container with social media links */}
 
-                    <div className="grid grid-cols-2 grid-rows-2 gap-10 place-items-center" >
+                    <div className="grid grid-cols-2 grid-rows-2 gap-10 place-items-center items-stretch cursor-pointer w-full px-8 md:px-16">
 
                         {/* Github Link */}
-                        <div className="flex flex-col items-center justify-center">
+                        <div className="flex flex-col items-center justify-center
+                        bg-[#fff5f9] dark:bg-[#120a1f] dark:text-white
+                        p-4 w-full rounded-xl transition duration-300
+                        hover:bg-[#dbc1ef]/70 dark:hover:bg-[#dbc1ef]/70
+                        hover:text-[#c38be5] dark:hover:text-[#c38be5]
+                        border-2 border-[#fff5f9] dark:border-[#120a1f]
+                        hover:border-2 hover:border-[#c38be5] dark:hover:border-[#c38be5]">
+
                             <FiGithub size={35}/>
-                            <p className="text-xl">GitHub</p>
+                            <a href="https://github.com/fernandasfarias" target="_blank" rel="noopener noreferrer">Github</a>
+
                         </div>
 
                         {/* Linkedin Link */}
-                        <div className="flex flex-col items-center justify-center">
+                        <div className="flex flex-col items-center justify-center bg-[#fff5f9] p-4 w-full rounded-xl dark:bg-[#120a1f] dark:text-white
+                       transition duration-300
+                       hover:bg-[#cce4ff]/70 dark:hover:bg-[#cce4ff]/70
+                       hover:text-[#66b3ff] dark:hover:text-[#66b3ff]
+                       border-2 border-[#fff5f9] dark:border-[#120a1f]
+                       hover:border-2 hover:border-[#66b3ff] dark:hover:border-[#66b3ff]">
+
                             <LuLinkedin size={35}/>
-                            <p className="text-xl">Linkedin</p>
+                            <a href="https://www.linkedin.com/in/fernandaafariass/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+
                         </div>
 
                         {/* Instagram Link */}
-                        <div className="flex flex-col items-center justify-center">
+                        <div className="flex flex-col items-center justify-center bg-[#fff5f9] p-4 w-full rounded-xl dark:bg-[#120a1f] dark:text-white
+                        transition duration-300
+                        hover:bg-[#fde6f1]/50 dark:hover:bg-[#fde6f1]/50
+                        hover:text-[#f39fcf] dark:hover:text-[#f39fcf]
+                        border-2 border-[#fff5f9] dark:border-[#120a1f]
+                        hover:border-2 hover:border-[#f39fcf] dark:hover:border-[#f39fcf]">
+
                             <FiInstagram size={35}/>
-                            <p className="text-xl">Instagram</p>
+                            <a href="https://www.instagram.com/nandasfarias_/" target="_blank" rel="noopener noreferrer">Instagram</a>
+
                         </div>
 
                         {/* e-mail link*/}
-                        <div className="flex flex-col items-center justify-center">
+                        <div className="flex flex-col items-center justify-center bg-[#fff5f9] p-4 w-full rounded-xl dark:bg-[#120a1f] dark:text-white
+                        transition duration-300
+                        hover:bg-[#dbc1ef]/70 dark:hover:bg-[#dbc1ef]/70
+                        hover:text-[#c38be5] dark:hover:text-[#c38be5]
+                        border-2 border-[#fff5f9] dark:border-[#120a1f] hover:border-2 hover:border-[#c38be5] dark:hover:border-[#c38be5]">
+
                             <MdOutlineEmail size={35}/>
-                            <p className="text-xl">E-mail</p>
+                            <a href="mailto:fernandafariass734@gmail.com">E-mail</a>
+
                         </div>
 
                     </div>
