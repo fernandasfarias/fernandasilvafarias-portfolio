fb91a1ce4c86e9645182edc116fcda0929170184
fernandasfarias <fernandafariass734@gmail.com>
2026-03-14 09:15:55 -0300

    fix(footer): change footer text

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -2,7 +2,7 @@ export default function(){
     return(
         <div className="bg-white text-black dark:bg-black dark:text-white
         flex flex-col items-center justify-center p-8">
-            <p>&copy; Fernanda 2026.</p>
+            <p>&copy; {new Date().getFullYear()} Fernanda. All rights reserved.</p>
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Start from './components/sections/Start';
 import About from './components/sections/About';
 import Projects from './components/sections/Projects';
 import Contact from './components/sections/Contact';
-import Footer from './components/sections/Footer';
+import Footer from './components/layouts/Footer';
 
 export default function Home() {
   return (
